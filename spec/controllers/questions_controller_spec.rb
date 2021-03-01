f7450b09c16190be75acf3fbe162b7f512acacd5
Alex Govorov <alex@govorov.pro>
2021-03-01 23:32:33 +0300

    QuestionsController POST #create tests is green

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -29,13 +29,30 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'save a new question in the database'
-      it 'redirects to show view'
+      it 'save a new question in the database' do
+        expect do
+          post :create, params: { question: attributes_for(:question) }
+        end.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
     end
 
     context 'with invalid attributes' do
-      it 'does not save the question'
-      it 're-renders new view'
+      it 'does not save the question' do
+        expect do
+          post :create,
+               params: { question: attributes_for(:question, :invalid) }
+        end.not_to change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
