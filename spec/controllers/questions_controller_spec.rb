8b27297d058ac053d565794e60b5362c15763c41
Alex Govorov <alex@govorov.pro>
2021-03-01 04:02:08 +0300

    QuestionsController #new and #show specs templates

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #new' do
+    it 'assigns a new Question to @question'
+    it 'renders new view'
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested question to @question'
+    it 'renders show view'
+  end
+
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'save a new question in the database'
