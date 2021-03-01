baba7f8c1373f5e572655e551a5b5885a1473802
Alex Govorov <alex@govorov.pro>
2021-03-01 03:43:04 +0300

    QuestionsController #create spec template

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new question in the database'
+      it 'redirects to show view'
+    end
 
+    context 'with invalid attributes' do
+      it 'does not save the question'
+      it 're-renders new view'
+    end
+  end
 end
