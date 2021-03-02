ed0921dbbc911b710d0cecf461b5f5fd9922bf7c
Alex Govorov <alex@govorov.pro>
2021-03-02 04:29:13 +0300

    AnswersController POST #create tests is green

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  describe 'GET #new' do
-    let(:question) { create(:question) }
+  let(:question) { create(:question) }
 
+  describe 'GET #new' do
     before { get :new, params: { question_id: question } }
 
     it 'assigns a new question.answers to @answer' do
@@ -17,13 +17,34 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'save a new answer in the database'
-      it 'redirects to question_path'
+      it 'save a new answer in the database' do
+        expect do
+          # Код с post запросом повторяется дважды, но 1ый раз внутри блока
+          # Возожно ли отрефакторить, чтобы исключить дублирование кода?
+          # Если вынести его в before, то не понятно какой объект передавать expect вместо блока
+          post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        end.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to question_path' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_path(question)
+      end
     end
 
     context 'with invalid attributes' do
-      it 'does not save the answer'
-      it 're-renders new view'
+      it 'does not save the answer' do
+        expect do
+          # В этом контексте аналогичная ситуация
+          post :create,
+               params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        end.not_to change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
